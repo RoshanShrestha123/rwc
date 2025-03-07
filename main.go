97b4da71a9261ff3f6730df2b8d102e50184ef89
Roshan Shrestha <roshan.shrestha4u@gmail.com>
2025-03-07 10:42:53 +0545

    word count basic logic

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	var wc int
+
+	args := os.Args
+	if len(args) < 2 {
+		log.Fatal("please provide the file name!")
+	}
+
+	fileName := args[1:2]
+	file, err := os.Open(fileName[0])
+	if err != nil {
+		panic(err)
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		totalWords := strings.Fields(line)
+		wc += len(totalWords)
+	}
+
+	fmt.Printf("Total words in files: %d\n", wc)
 }
